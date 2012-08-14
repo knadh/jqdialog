fe3c483540e3a9d77b1f6c29d1b75a586a7dc060
Andreyco <contact@andrejbadin.com>
2012-08-14 13:01:47 +0200

    Added ability to use extra overlay layer
    User is able to chose whether to use overlay behind any jqDialog_box,
    set it's background color as well.

--- jqdialog.js
+++ jqdialog.js
@@ -5,7 +5,7 @@
 	http://kailashnadh.name/code/jqdialog
 
 	License: GPL
-	
+
 	v1.4	September 21 2011	-	Added support for Enter and Escape keyboard shortcuts.
 	v1.3.1	April 4 2011	-	Fixed an IE compatibility issue. Thanks to Filip Vojtisek.
 	v1.3	February 6 2011	-	Rewrote the whole plugin to comply with jQuery's plugin standards
@@ -13,7 +13,7 @@
 **/
 
 (function($) {
-	
+
 	var t = null;
 	$.jqDialog = {
 		escape_callback: null,
@@ -29,6 +29,7 @@
 
 		//________element ids
 		ids: {
+			div_overlay: 'jqDialog_overlay',
 			div_box:	'jqDialog_box',
 			div_content:	'jqDialog_content',
 			div_options: 'jqDialog_options',
@@ -38,18 +39,24 @@
 			bt_ancel: 'jqDialog_ok',
 			input: 'jqDialog_input'
 		},
-		
+
+		//________defaults
+		defaults: {
+			overlay: true,
+			overlayColor: 'rgba(0,0,0,0.75)'
+		},
+
 		//________confirm dialog
 		confirm: function(message, callback_yes, callback_no) {
 			t.create(message);
-			
+
 			t.parts.bt_ok.hide();
 			t.parts.bt_cancel.hide();
-			
+
 			t.parts.bt_yes.show();
 			t.parts.bt_no.show();
 			t.parts.bt_yes.focus();
-			
+
 			// just redo this everytime in case a new callback is presented
 			t.parts.bt_yes.unbind().click( function() {
 				t.cleanKeypressCallbacks();
@@ -71,7 +78,7 @@
 				if(callback_no) callback_no();
 			};
 		},
-		
+
 		//________prompt dialog
 		prompt: function(message, content, callback_ok, callback_cancel) {
 			t.create(
@@ -79,16 +86,16 @@
 					append(message)
 					.append( $("<div>").append( t.parts.input.val(content) ) )
 			);
-			
+
 			// activate appropriate controls
 			t.parts.bt_yes.hide();
 			t.parts.bt_no.hide();
 
 			t.parts.bt_ok.show();
-			t.parts.bt_cancel.show(); 
-			
+			t.parts.bt_cancel.show();
+
 			t.parts.input.focus();
-			
+
 			// just redo t everytime in case a new callback is presented
 			t.parts.bt_ok.unbind().click( function() {
 				t.cleanKeypressCallbacks();
@@ -98,7 +105,7 @@
 			t.enter_callback = function() {
 				if(callback_ok) callback_ok( t.parts.input.val() );
 			};
-			
+
 			t.parts.bt_cancel.unbind().click( function() {
 				t.cleanKeypressCallbacks();
 				t.close();
@@ -108,7 +115,7 @@
 				if(callback_cancel) callback_cancel();
 			};
 		},
-		
+
 		//________alert dialog
 		alert: function(content, callback_ok) {
 			t.create(content);
@@ -117,11 +124,11 @@
 			t.parts.bt_cancel.hide();
 			t.parts.bt_yes.hide();
 			t.parts.bt_no.hide();
-			
+
 			t.parts.bt_ok.show();
-			
+
 			t.parts.bt_ok.focus();
-			
+
 			// just redo this everytime in case a new callback is presented
 			t.parts.bt_ok.unbind().click( function() {
 				t.cleanKeypressCallbacks();
@@ -153,18 +160,28 @@
 
 		//________create a dialog box
 		create: function(content) {
+
 			t.check();
-			
+
 			t.maintainPosition( t.parts.div_box );
-			
+
 			clearTimeout(t.close_timer);
 			t.parts.div_content.html(content);
 			t.parts.div_options.show();
-			t.parts.div_box.fadeIn('fast');
+			if(t.defaults.overlay){
+				t.parts.div_box.show();
+				t.parts.div_overlay.fadeIn('fast');
+			}else{
+				t.parts.div_box.fadeIn('fast');
+			}
 		},
 		//________close the dialog box
 		close: function() {
+			if(t.defaults.overlay){
+				t.parts.div_overlay.fadeOut('fast');
+			}
 			t.parts.div_box.fadeOut('fast');
+
 			t.clearPosition();
 		},
 
@@ -179,10 +196,12 @@
 			});
 		},
 		maintainPosition: function(object) {
+
 			t.makeCenter(object);
-			
+
 			$(window).bind('scroll.jqDialog', function() {
-				t.makeCenter(object);
+				if(!t.defaults.overlay)
+					t.makeCenter(object);
 			} );
 		},
 
@@ -194,13 +213,19 @@
 			else {
 				t.init_done = true;
 			}
-			
-			$('body').append( t.parts.div_box );
+
+			if(t.defaults.overlay)
+			{
+				$('body').append( t.parts.div_overlay );
+			}else{
+				$('body').append( t.parts.div_box );
+			}
 		},
 		init: function() {
 			t.parts = {};
-			
+
 			// create the dialog components
+			t.parts.div_overlay = $("<div>").attr({ id: t.ids.div_overlay }).css({ background: t.defaults.overlayColor} );
 			t.parts.div_box = $("<div>").attr({ id: t.ids.div_box });
 			t.parts.div_content = $("<div>").attr({ id: t.ids.div_content });
 			t.parts.div_options = $("<div>").attr({ id: t.ids.div_options });
@@ -215,26 +240,44 @@
 			// assemble the parts
 			t.parts.div_box.append( t.parts.div_content )
 					.append(
-						t.parts.div_options.append(t.parts.bt_yes)
-										   .append(t.parts.bt_no)
-										   .append(t.parts.bt_ok)
-										   .append(t.parts.bt_cancel)
+						t.parts.div_options
+							.append(t.parts.bt_yes)
+							.append(t.parts.bt_no)
+							.append(t.parts.bt_ok)
+							.append(t.parts.bt_cancel)
 					);
 
-			// add to body
-			t.parts.div_box.hide();
-			
+			if(t.defaults.overlay)
+			{
+				t.parts.div_overlay.append( t.parts.div_box );
+				t.parts.div_overlay.hide();
+			} else {
+				// add to body
+				t.parts.div_box.hide();
+			}
+
 			// keyboard bindings
 			$(document).keyup(function(e) {
 				if(e.altKey) return;
 
-				if (e.keyCode == 13) {
+				if (e.keyCode === 13) {
 					t.enterPressed();
 				}
-				if (e.keyCode == 27) {
+				if (e.keyCode === 27) {
 					t.escapePressed();
 				}
 			});
+		},
+		reinit: function()
+		{
+			t.parts.div_overlay.remove();
+			t.parts.div_box.remove();
+			t.init_done = false;
+			t.init();
+		},
+		setDefaults: function(options){
+			t.defaults = $.extend(t.defaults, options);
+			t.reinit();
 		},
 		cleanKeypressCallbacks: function() {
 			t.enter_callback = null;
@@ -255,7 +298,7 @@
 				t.enter_callback();
 				t.enter_callback = null;
 			}
-		},
+		}
 	};
 	t = $.jqDialog;
 	$.jqDialog.init();
